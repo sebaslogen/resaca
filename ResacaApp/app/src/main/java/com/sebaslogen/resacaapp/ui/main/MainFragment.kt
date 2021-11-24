960500119371615d94185014acc696040556fa51
sebas <sebastian@q42.nl>
2021-11-24 14:23:46 +0100

    Add template Android App with Fragment

---
+++ ResacaApp/app/src/main/java/com/sebaslogen/resacaapp/ui/main/MainFragment.kt
@@ -0,0 +1,28 @@
+package com.sebaslogen.resacaapp.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sebaslogen.resacaapp.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this).get(MainViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+}
---
+++ ResacaApp/app/src/main/java/com/sebaslogen/resacaapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.sebaslogen.resacaapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
