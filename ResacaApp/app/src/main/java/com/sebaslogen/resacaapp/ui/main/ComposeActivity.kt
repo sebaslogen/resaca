fb1298435414e28707e87c2c4ae5b3410a645903
sebas <sebastian@q42.nl>
2021-11-25 23:58:40 +0100

    Add Compose navigation to different compose destinations

--- ResacaApp/app/src/main/java/com/sebaslogen/resacaapp/ui/main/ComposeActivity.kt
+++ ResacaApp/app/src/main/java/com/sebaslogen/resacaapp/ui/main/ComposeActivity.kt
@@ -3,11 +3,19 @@ package com.sebaslogen.resacaapp.ui.main
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.sebaslogen.resaca.compose.installScopedViewModelContainer
+import com.sebaslogen.resacaapp.ui.main.compose.DemoScopedObjectComposable
+import com.sebaslogen.resacaapp.ui.main.compose.DemoScopedViewModelComposable
 import com.sebaslogen.resacaapp.ui.main.ui.theme.ResacaAppTheme
 
 class ComposeActivity : ComponentActivity() {
@@ -17,7 +25,7 @@ class ComposeActivity : ComponentActivity() {
             ResacaAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    ScreensWithNavigation()
                 }
             }
         }
@@ -25,14 +33,41 @@ class ComposeActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ScreensWithNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "first") {
+        composable("first") {
+            it.installScopedViewModelContainer()
+            DemoScopedObjectComposable()
+            DemoScopedViewModelComposable()
+            NavigationButtons(navController)
+        }
+        composable("second") {
+            it.installScopedViewModelContainer()
+            DemoScopedObjectComposable()
+            DemoScopedViewModelComposable()
+            NavigationButtons(navController)
+        }
+    }
+}
+
+@Composable
+fun NavigationButtons(navController: NavHostController) {
+    Button(onClick = { navController.navigate("first") }) {
+        Text(text = "Push first destination")
+    }
+    Button(onClick = { navController.navigate("second") }) {
+        Text(text = "Push second destination")
+    }
+    Button(onClick = { navController.popBackStack() }) {
+        Text(text = "Go to BACK")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ResacaAppTheme {
-        Greeting("Android")
+        ScreensWithNavigation()
     }
 }
