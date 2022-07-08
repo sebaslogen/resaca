dcc4bf65cf90a0f5d1e7d011b7e3d06957ac6ce9
sebas <sebastian@q42.nl>
2022-07-08 11:04:24 +0200

    Improve public API documentation

--- resaca/src/main/java/com/sebaslogen/resaca/ScopedMemoizers.kt
+++ resaca/src/main/java/com/sebaslogen/resaca/ScopedMemoizers.kt
@@ -21,9 +21,13 @@ import java.util.*
 
 /**
  * Return an object created with the provided [builder] function and store this object
- * in the [ScopedViewModelContainer] which will keep this object in memory for as long as needed.
- * Internally, a key will be generated for this object in the Compose tree and if an object is present
- * for this key in the [ScopedViewModelContainer], then it will be returned instead of calling [builder]
+ * in the [ScopedViewModelContainer] which will keep this object in memory for as long as needed,
+ * and until the requester Composable is permanently disposed of.
+ * This means, it retains the object across recompositions, during configuration changes, and
+ * also when the container Fragment or Compose Navigation destination goes into the backstack.
+ *
+ * Internally, an extra key will be generated for this object in the Compose tree and if an object is present
+ * for this key in the [ScopedViewModelContainer], then it will be returned instead of calling the [builder].
  *
  * @param key Key to track the version of the store object. Changing [key] between compositions will produce and remember a new value by calling [builder].
  * @param builder Factory function to produce a new value that will be remembered.
@@ -45,12 +49,15 @@ fun <T : Any> rememberScoped(key: Any? = null, builder: @DisallowComposableCalls
 
 /**
  * Return a [ViewModel] provided by the default [ViewModelProvider.Factory] and a [ViewModelProvider].
+ * The [ViewModel] will keep in memory for as long as needed, and until the requester Composable is permanently disposed of.
+ * This means, it retains the [ViewModel] across recompositions, during configuration changes, and
+ * also when the container Fragment or Compose Navigation destination goes into the backstack.
  *
  * The [ViewModel] will be created and stored by the [ViewModelProvider] using a default [ViewModelProvider.Factory] and a [ViewModelStore].
  * The [ScopedViewModelOwner] will be the object stored in the [ScopedViewModelContainer] and
  * the [ScopedViewModelContainer] will be in charge of keeping the [ScopedViewModelOwner] and its [ViewModel] in memory for as long as needed.
  *
- * Internally, a key will be generated for this [ScopedViewModelOwner] in the Compose tree and if a [ScopedViewModelOwner] is present
+ * Internally, an extra key will be generated for this [ScopedViewModelOwner] in the Compose tree and if a [ScopedViewModelOwner] is present
  * for this key in the [ScopedViewModelContainer], then it will be used to invoke [ViewModelProvider] to return an existing [ViewModel],
  * instead of creating a new [ScopedViewModelOwner] that produces a new [ViewModel] instance when the keys don't match.
  *
@@ -78,7 +85,7 @@ inline fun <reified T : ViewModel> viewModelScoped(key: Any? = null): T {
  * The [ScopedViewModelOwner] will be the object stored in the [ScopedViewModelContainer] and
  * the [ScopedViewModelContainer] will be in charge of keeping the [ScopedViewModelOwner] and its [ViewModel] in memory for as long as needed.
  *
- * Internally, a key will be generated for this [ScopedViewModelOwner] in the Compose tree and if a [ScopedViewModelOwner] is present
+ * Internally, an extra key will be generated for this [ScopedViewModelOwner] in the Compose tree and if a [ScopedViewModelOwner] is present
  * for this key in the [ScopedViewModelContainer], then it will be used to invoke [ViewModelProvider] to return an existing [ViewModel],
  * instead of creating a new [ScopedViewModelOwner] that produces a new [ViewModel] instance when the keys don't match.
  *
