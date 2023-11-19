8b34cfe3ca794f764d6e4ac4d33259b64e91ec41
sebaslogen <neoranga55@gmail.com>
2023-11-19 11:56:56 +0100

    Update public API contracts and fix test

--- sample/src/test/java/com/sebaslogen/resacaapp/sample/ComposeActivityRecreationTests.kt
+++ sample/src/test/java/com/sebaslogen/resacaapp/sample/ComposeActivityRecreationTests.kt
@@ -15,6 +15,8 @@ import com.sebaslogen.resacaapp.sample.ui.main.rememberScopedDestination
 import com.sebaslogen.resacaapp.sample.ui.main.showSingleScopedViewModel
 import com.sebaslogen.resacaapp.sample.ui.main.viewModelScopedDestination
 import com.sebaslogen.resacaapp.sample.utils.ComposeTestUtils
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -92,15 +94,17 @@ class ComposeActivityRecreationTests : ComposeTestUtils {
                 showSingleScopedViewModel = false // This is a fake night-mode change but it will remove Composable after Activity re-creation
                 activity.recreate()
                 printComposeUiTreeToLog()
-                Thread.sleep(COMPOSITION_RESUMED_TIMEOUT_IN_SECONDS * 1000) // Wait for the ViewModel to be cleared
-                printComposeUiTreeToLog() // Second print is needed to push the main thread forward
-                val finalAmountOfViewModelsCleared = viewModelsClearedGloballySharedCounter.get()
+                runBlocking {
+                    delay(COMPOSITION_RESUMED_TIMEOUT_IN_SECONDS * 1000) // Wait for the ViewModel to be cleared
+                    printComposeUiTreeToLog() // Second print is needed to push the main thread forward
+                    val finalAmountOfViewModelsCleared = viewModelsClearedGloballySharedCounter.get()
 
-                // Then the scoped ViewModel disappears
-                onNodeWithTestTag("FakeInjectedViewModel Scoped", assertDisplayed = false).assertDoesNotExist()
-                assert(finalAmountOfViewModelsCleared == initialAmountOfViewModelsCleared + 1) {
-                    "The amount of FakeInjectedViewModel that were cleared after key change ($finalAmountOfViewModelsCleared) " +
-                            "was not higher that the amount before the key change ($initialAmountOfViewModelsCleared)"
+                    // Then the scoped ViewModel disappears
+                    onNodeWithTestTag("FakeInjectedViewModel Scoped", assertDisplayed = false).assertDoesNotExist()
+                    assert(finalAmountOfViewModelsCleared == initialAmountOfViewModelsCleared + 1) {
+                        "The amount of FakeInjectedViewModel that were cleared after key change ($finalAmountOfViewModelsCleared) " +
+                                "was not higher that the amount before the key change ($initialAmountOfViewModelsCleared)"
+                    }
                 }
             }
         }
