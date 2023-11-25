b9f49fdf38776f7f33feea6debd39d1c1538cd71
sebaslogen <neoranga55@gmail.com>
2023-11-25 19:04:35 +0100

    Add documentation for new KeyInScopeResolver feature in all versions of rememberScoped

--- resacakoin/src/main/java/com/sebaslogen/resaca/koin/ScopedMemoizers.kt
+++ resacakoin/src/main/java/com/sebaslogen/resaca/koin/ScopedMemoizers.kt
@@ -20,8 +20,29 @@ import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
 import org.koin.core.scope.Scope
 
+
 /**
- * TODO: Docs and how to use
+ * Return a [ViewModel] provided by a Koin [ViewModelProvider.Factory] and a [ViewModelProvider].
+ * The [ViewModel] will keep in memory for as long as needed, and until the requester Composable is permanently gone
+ * and the [key] is not present anymore in [keyInScopeResolver] or the [keyInScopeResolver] itself leaves Composition.
+ * This means, it retains the [ViewModel] across recompositions, during configuration changes, and
+ * also when the container Fragment or Compose Navigation destination goes into the backstack.
+ *
+ * The returned [ViewModel] is provided by the [ViewModelProvider] using a Koin [ViewModelProvider.Factory] and a [ViewModelStore].
+ * The [ViewModel] will be created and stored by the [ViewModelProvider] in the [ViewModelStore].
+ * The [ScopedViewModelOwner] will be the object stored in the [ScopedViewModelContainer] and
+ * the [ScopedViewModelContainer] will be in charge of keeping the [ScopedViewModelOwner] and its [ViewModel] in memory for as long as needed.
+ *
+ * Internally, a key will be generated for this [ScopedViewModelOwner] in the Compose tree and if a [ScopedViewModelOwner] is present
+ * for this key in the [ScopedViewModelContainer], then it will be used to invoke [ViewModelProvider] to return an existing [ViewModel],
+ * instead of creating a new [ScopedViewModelOwner] that produces a new [ViewModel] instance when the keys don't match.
+ *
+ * @param key Key to track the version of the [ViewModel]. Changing [key] between compositions will produce and store a new [ViewModel].
+ * @param keyInScopeResolver A function that uses [key] to determine if the ViewModel should be kept in memory even after it's no longer part of the composition.
+ * @param qualifier Koin qualifier to help qualify a component, like named qualifiers.
+ * @param scope Koin scope.
+ * @param parameters for instance building injection. These can be used for assisted injection.
+ * @param defaultArguments A [Bundle] containing all the default arguments that will be provided to the [ViewModel]. VM still has to manually read these values.
  */
 @OptIn(KoinInternalApi::class)
 @Composable
@@ -45,6 +66,9 @@ public inline fun <reified T : ViewModel, K : Any> koinViewModelScoped(
 
 /**
  * Return a [ViewModel] provided by a Koin [ViewModelProvider.Factory] and a [ViewModelProvider].
+ * The [ViewModel] will keep in memory for as long as needed, and until the requester Composable is permanently gone.
+ * This means, it retains the [ViewModel] across recompositions, during configuration changes, and
+ * also when the container Fragment or Compose Navigation destination goes into the backstack.
  *
  * The returned [ViewModel] is provided by the [ViewModelProvider] using a Koin [ViewModelProvider.Factory] and a [ViewModelStore].
  * The [ViewModel] will be created and stored by the [ViewModelProvider] in the [ViewModelStore].
