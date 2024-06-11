b147056e47060d46768d986846dcdf803563b2f4
Sebastian Lobato Genco <sebastian@q42.nl>
2024-06-11 14:23:01 +0200

    Improve proguard optimizations for Android

--- resaca/build.gradle.kts
+++ resaca/build.gradle.kts
@@ -20,7 +20,7 @@ android {
     buildTypes {
         getByName("release") {
             isMinifyEnabled = false
-            proguardFiles("proguard-rules.pro")
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
     compileOptions {
