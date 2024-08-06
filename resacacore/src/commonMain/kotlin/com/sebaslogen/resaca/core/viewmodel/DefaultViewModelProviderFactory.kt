f07ecaa6eb4d908d157706267c6828a98747974f
Sebastian Lobato Genco <sebastian@q42.nl>
2024-08-06 10:44:03 +0200

    Add next step ToDo

--- resacacore/src/commonMain/kotlin/com/sebaslogen/resaca/core/viewmodel/DefaultViewModelProviderFactory.kt
+++ resacacore/src/commonMain/kotlin/com/sebaslogen/resaca/core/viewmodel/DefaultViewModelProviderFactory.kt
@@ -3,3 +3,6 @@ package com.sebaslogen.resaca.core.viewmodel
 import androidx.lifecycle.ViewModelProvider
 
 internal expect object DefaultViewModelProviderFactory : ViewModelProvider.Factory
+
+
+// TODO: Make resaca KMP and move all code from CORE to resaca
