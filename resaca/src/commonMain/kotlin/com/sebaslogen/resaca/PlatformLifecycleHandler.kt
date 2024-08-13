56b78317f416147fdda631559cc2d2e53335dd97
Sebastian Lobato Genco <sebastian@q42.nl>
2024-08-13 17:40:41 +0200

    Add docs and port rememberKeysInScope to multiplatform

--- resaca/src/commonMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.kt
+++ resaca/src/commonMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.kt
@@ -2,6 +2,10 @@ package com.sebaslogen.resaca
 
 public const val COMPOSITION_RESUMED_TIMEOUT_IN_SECONDS: Long = 1
 
+/**
+ * This class allows a specific platform to handle the lifecycle of objects stored in [ScopedViewModelContainer].
+ * For example, in Android, this class is used to wait for the first frame after Activity resumes and properly handle changes after configuration changes.
+ */
 expect public class PlatformLifecycleHandler() {
     public fun onResumed()
     public fun onDestroyed()
