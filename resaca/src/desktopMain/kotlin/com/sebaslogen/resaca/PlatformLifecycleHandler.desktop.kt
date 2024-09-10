22ca03b568b84d59f61b52dac57c79ebfbdc3091
sebaslogen <neoranga55@gmail.com>
2024-09-10 14:29:47 +0200

    Fix default ViewModel factory for JVM Desktop

--- resaca/src/desktopMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.desktop.kt
+++ resaca/src/desktopMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.desktop.kt
@@ -1,18 +1,6 @@
 package com.sebaslogen.resaca
 
 /**
- * There is no need to handle the lifecycle of objects in iOS, so this class is a no-op.
+ * There is no need to handle the lifecycle of objects in Desktop, so this class is a no-op.
  */
-public actual class PlatformLifecycleHandler {
-    public actual fun onResumed() {
-        // No-op
-    }
-
-    public actual fun onDestroyed() {
-        // No-op
-    }
-
-    public actual suspend fun awaitBeforeDisposing(inForeground: Boolean) {
-        // No-op
-    }
-}
+public actual typealias PlatformLifecycleHandler = DefaultPlatformLifecycleHandler
--- resaca/src/desktopMain/kotlin/com/sebaslogen/resaca/viewmodel/DefaultViewModelProviderFactory.desktop.kt
+++ resaca/src/desktopMain/kotlin/com/sebaslogen/resaca/viewmodel/DefaultViewModelProviderFactory.desktop.kt
@@ -1,8 +1,14 @@
 package com.sebaslogen.resaca.viewmodel
 
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import kotlin.reflect.KClass
 
 /**
- * This object provides a default implementation of [ViewModelProvider.Factory] that creates a new instance of a ViewModel using the default constructor.
+ * Default implementation of [ViewModelProvider.Factory] that creates a new instance of a ViewModel using the default constructor.
  */
-internal actual object DefaultViewModelProviderFactory : ViewModelProvider.Factory
+internal actual object DefaultViewModelProviderFactory : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: KClass<T>, extras: CreationExtras): T =
+        JvmViewModelProviders.createViewModel(modelClass.java)
+}
