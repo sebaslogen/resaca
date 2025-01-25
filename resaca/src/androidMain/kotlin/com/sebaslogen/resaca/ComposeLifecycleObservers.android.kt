4bf6f5b500619e2deaf94904abdfd48c26a6ed39
sebaslogen <neoranga55@gmail.com>
2025-01-25 17:11:38 +0100

    Update Activity lib and use LocalActivity instead of context

--- resaca/src/androidMain/kotlin/com/sebaslogen/resaca/ComposeLifecycleObservers.android.kt
+++ resaca/src/androidMain/kotlin/com/sebaslogen/resaca/ComposeLifecycleObservers.android.kt
@@ -1,22 +1,20 @@
 package com.sebaslogen.resaca
 
-import android.app.Activity
-import android.content.Context
-import android.content.ContextWrapper
+import androidx.activity.compose.LocalActivity
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.RememberObserver
 import androidx.compose.runtime.remember
-import androidx.compose.ui.platform.LocalContext
 
 @Composable
 @PublishedApi
 internal actual fun ObserveComposableContainerLifecycle(scopedViewModelContainer: ScopedViewModelContainer) {
     // Observe state of configuration changes when disposing
-    val context = LocalContext.current
-    remember(context) {
+    val activity = LocalActivity.current
+        ?: throw IllegalStateException("Expected an Activity for detecting configuration changes for a NavBackStackEntry but instead found null")
+    remember(activity) {
         object : RememberObserver {
             private fun onRemoved() {
-                scopedViewModelContainer.setIsChangingConfiguration(context.findActivity().isChangingConfigurations)
+                scopedViewModelContainer.setIsChangingConfiguration(activity.isChangingConfigurations)
             }
 
             override fun onAbandoned() {
@@ -32,17 +30,4 @@ internal actual fun ObserveComposableContainerLifecycle(scopedViewModelContainer
             }
         }
     }
-}
-
-internal fun Context.findActivity(): Activity {
-    var ctx = this
-    while (ctx is ContextWrapper) {
-        if (ctx is Activity) {
-            return ctx
-        }
-        ctx = ctx.baseContext
-    }
-    throw IllegalStateException(
-        "Expected an activity context for detecting configuration changes for a NavBackStackEntry but instead found: $ctx"
-    )
 }
