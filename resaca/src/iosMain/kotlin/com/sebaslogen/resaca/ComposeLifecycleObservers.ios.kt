5dde850a9c72824c442644eb3973a9d848ee2979
Sebastian Lobato Genco <sebastian@q42.nl>
2025-03-25 14:24:06 +0100

    Fix API ootIn

--- resaca/src/iosMain/kotlin/com/sebaslogen/resaca/ComposeLifecycleObservers.ios.kt
+++ resaca/src/iosMain/kotlin/com/sebaslogen/resaca/ComposeLifecycleObservers.ios.kt
@@ -1,6 +1,9 @@
+@file:OptIn(ResacaPackagePrivate::class)
+
 package com.sebaslogen.resaca
 
 import androidx.compose.runtime.Composable
+import com.sebaslogen.resaca.utils.ResacaPackagePrivate
 
 @Composable
 @PublishedApi
