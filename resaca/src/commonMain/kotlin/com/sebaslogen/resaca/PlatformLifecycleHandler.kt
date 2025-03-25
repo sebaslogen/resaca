c1a7b5161bb1cabab2aec746769e75956757cf35
sebaslogen <neoranga55@gmail.com>
2025-03-25 16:01:15 +0100

    Add docs

--- resaca/src/commonMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.kt
+++ resaca/src/commonMain/kotlin/com/sebaslogen/resaca/PlatformLifecycleHandler.kt
@@ -2,6 +2,10 @@ package com.sebaslogen.resaca
 
 import com.sebaslogen.resaca.utils.ResacaPackagePrivate
 
+/**
+ * Timeout in seconds to wait for the composition to be resumed after the Activity/Fragment is resumed.
+ * This is only applicable to Android.
+ */
 @ResacaPackagePrivate
 public const val COMPOSITION_RESUMED_TIMEOUT_IN_SECONDS: Long = 1
 
