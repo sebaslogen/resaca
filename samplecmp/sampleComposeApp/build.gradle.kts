abd210f43b3b6fab14118afc9a0e651f463e697f
sebaslogen <neoranga55@gmail.com>
2025-03-25 10:59:30 +0100

    Fix deprecated and hopefully flaky test

--- samplecmp/sampleComposeApp/build.gradle.kts
+++ samplecmp/sampleComposeApp/build.gradle.kts
@@ -45,7 +45,7 @@ kotlin {
     }
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        moduleName = "composeApp"
+        outputModuleName = "composeApp"
         browser {
             val projectDirPath = project.projectDir.path
             commonWebpackConfig {
